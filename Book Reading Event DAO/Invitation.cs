7eb1b847a73d053f418410ae80f9e94ae0418c1a
Piyush Saini <155855074+beebusy03@users.noreply.github.com>
2024-01-07 18:52:17 +0530

    Data Access Layer

---
+++ Book Reading Event DAO/Invitation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO
+{
+    public class Invitation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int InvitationActive { get; set; }
+
+        public int EventId { get; set; }
+        public int UserId { get; set; }
+
+
+        public virtual Event Event { get; set; }
+
+       // public virtual User User { get; set; }
+    }
+}
---
+++ Book Reading Event DAO/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Reading_Event_DAO
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string UserEmail { get; set; }
+
+        [Required]
+        public string UserPassword { get; set; }
+
+        public string UserRole { get; set; }
+
+        // public virtual IList<Comment> Comments { get; set; }
+
+        public virtual IList<Event> Events { get; set; }
+    }
+}
